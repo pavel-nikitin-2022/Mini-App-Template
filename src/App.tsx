b808f16c010b21d9bf70a09b1dfaa6201e9540f7
Pasha Nikitin <pasha-nikitin-2022@mail.ru>
2023-04-20 04:15:25 +0300

    drop/paste/choose feature: add @rpldy/uploady lib feature: add simple ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import React, { useState } from 'react'
-import {
-  View,
-  ScreenSpinner,
-  SplitLayout,
-  SplitCol,
-  Panel,
-} from '@vkontakte/vkui'
+import React from 'react'
+import { View, SplitLayout, SplitCol, Panel } from '@vkontakte/vkui'
+
 import '@vkontakte/vkui/dist/vkui.css'
+import './global.css'
+import { Loader } from './components'
 
 const App = (): JSX.Element => {
-  const [popout] = useState(<ScreenSpinner size="large" />)
+  const [popout] = React.useState(<></>)
 
   return (
     <SplitLayout popout={popout}>
       <SplitCol>
         <View activePanel="main">
-          <Panel id="main"></Panel>
+          <Panel id="main">
+            <Loader />
+          </Panel>
         </View>
       </SplitCol>
     </SplitLayout>
